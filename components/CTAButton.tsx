bddaff0cc53507a9d3357092c7db151d0da5160e
Jaxon Scott <jaxonscott@Jaxons-MacBook-Pro.local>
2023-03-29 15:18:53 -0400

    added product component to home page

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -2,6 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export default function CTAButton(props: PropsWithChildren) {
   return (
-    <button className="bg-secondary font-semibold text-background rounded-md px-8 py-3.5">{props.children}</button>
+    <button className="bg-secondary font-semibold text-background rounded-md px-8 py-3.5 ">
+      {props.children}
+    </button>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import CTAButton from "../components/CTAButton";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import Image from "next/image";
+import ProductItem from "../components/ProductItem";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-3/4">
       <Navbar />
-      <div className="mt-14"></div>
-      <div>
-        <div className="w-1/2">
+      <div className="mt-20"></div>
+      <div className="flex gap-8">
+        <div className="w-1/2 h-3/4">
           <h1 className="text-6xl">
             Quality Caffeine <br /> Without the price tag
           </h1>
@@ -19,7 +21,7 @@ export default function Home() {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.{" "}
           </p>
-          <div className="mt-7"></div>
+          <div className="mt-10"></div>
           <div className="flex gap-9">
             <CTAButton>Browse our products</CTAButton>
             <Link
@@ -32,8 +34,8 @@ export default function Home() {
               </span>
             </Link>
           </div>
-          <div className="mt-14"></div>
-          <div className="w-full flex border p-6 gap-2 border-lighttext rounded-lg">
+          <div className="mt-24"></div>
+          <div className="w-full  flex border p-6 gap-2 border-lighttext rounded-lg">
             <p className="w-1/4 my-auto text-5xl font-extralight">10Y</p>
             <p className=" w-2/3 ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -41,6 +43,25 @@ export default function Home() {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.{" "}
             </p>
+          </div>
+        </div>
+        <div className="w-1/2 flex gap-4">
+          <div className="w-full h-full ">
+            <div className="h-full w-full">
+              <ProductItem />
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            <div className="w-full h-full flex ">
+              <div className="h-full w-full">
+                <ProductItem />
+              </div>
+            </div>
+            <div className="w-full h-full flex ">
+              <div className="h-full w-full">
+                <ProductItem />
+              </div>
+            </div>
           </div>
         </div>
       </div>
