ea2f154c1abc90a142394e56653e9689853dde56
Jaxon Scott <jaxonscott@Jaxons-MacBook-Pro.local>
2023-03-29 13:09:55 -0400

    Buttons and home page message added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { Poppins } from "@next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["200", "300", "400", "500", "600", "700"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import CTAButton from "../components/CTAButton";
 import Navbar from "../components/Navbar";
+import Link from "next/link";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -8,7 +10,7 @@ export default function Home() {
       <div className="mt-14"></div>
       <div>
         <div className="w-1/2">
-          <h1 className="text-5xl">
+          <h1 className="text-6xl">
             Quality Caffeine <br /> Without the price tag
           </h1>
           <p className="mt-6 leading-loose text-md ">
@@ -18,9 +20,30 @@ export default function Home() {
             aliquip ex ea commodo consequat.{" "}
           </p>
           <div className="mt-7"></div>
-          <CTAButton>Browse our products</CTAButton>
+          <div className="flex gap-9">
+            <CTAButton>Browse our products</CTAButton>
+            <Link
+              href={"/store"}
+              className="pt-3 font-light text-lg flex gap-2"
+            >
+              View Spring Flavours{" "}
+              <span className="pt-1.5">
+                <AiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="mt-14"></div>
+          <div className="w-full flex border p-6 gap-2 border-lighttext rounded-lg">
+            <p className="w-1/4 my-auto text-5xl font-extralight">10Y</p>
+            <p className=" w-2/3 ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  ); 
+  );
 }
