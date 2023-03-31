590a508ea718030ce806534ad59f357407ad9ed3
Jaxon Scott <jaxonscott@Jaxons-MacBook-Pro.local>
2023-03-31 15:56:47 -0400

    changed width for product cards on store page

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -13,8 +13,8 @@ export default function StorePage() {
         <div className="mb-4">
           <p className="font-light text-texthover">8 products</p>
         </div>
-        <div className="flex gap-4">
-          <div className=" w-1/4 py-32 relative ">
+        <div className="flex gap-4 flex-wrap">
+          <div className="w-full  md:w-1/4 py-32 relative ">
             <Image
               className="rounded-md"
               alt="product img"
@@ -33,7 +33,7 @@ export default function StorePage() {
             </div>
             <div></div>
           </div>
-          <div className=" w-1/4 py-32 relative ">
+          <div className=" w-full md:w-1/4 py-32 relative ">
             <Image
               className="rounded-md"
               alt="product img"
@@ -52,25 +52,6 @@ export default function StorePage() {
             </div>
             <div></div>
           </div>
-          <div className=" w-1/4 py-32 relative ">
-            <Image
-              className="rounded-md"
-              alt="product img"
-              src={product2}
-              layout="fill"
-              objectFit="cover"
-            />
-            <div className="absolute  flex bottom-0 px-4 bg-background bg-opacity-95 gap-2 w-full py-8">
-              <div>
-                <h1 className="text-lg font-semibold">House Specail</h1>
-                <p className="text-sm font-light">Our house Specail beans</p>
-              </div>
-              <div className="my-auto">
-                <h1 className="text-xl">$44</h1>
-              </div>
-            </div>
-          </div>
-          <div></div>
         </div>
       </div>
     </MotionWrapper>
