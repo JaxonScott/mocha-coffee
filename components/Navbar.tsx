73f7667bef5d3fae69751cb1153e6f58184d6b64
Jaxon Scott <jaxonscott@Jaxons-MacBook-Pro.local>
2023-03-31 10:10:29 -0400

    added transition to mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import logo from "public/images/ logo.png";
 import NavItem from "./NavItem";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Menu, Transition } from "@headlessui/react";
 import { clsx } from "clsx";
 
 export default function Navbar() {
@@ -70,47 +71,59 @@ export default function Navbar() {
           </button>
         </div>
       </div>
+
       <div className={clsx({ ["hidden"]: !isOpen, ["relative"]: isOpen })}>
         <div className="absolute bg-background w-full py-10 rounded-b-md">
-          <ul className="text-lighttext flex gap-4 pt-3 flex-col">
-            <Link href="/">
-              <NavItem>
-                {currentPath === "" ? (
-                  <span className="text-secondary">HOME</span>
-                ) : (
-                  "HOME"
-                )}
-              </NavItem>
-            </Link>
-            <Link href="/store">
+          <Transition
+            // as={Fragment}
+            show={isOpen}
+            enter="transition ease-out duration-400"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition-opacity duration-150"
+            leaveFrom=" opacity-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <ul className="text-lighttext flex gap-4 pt-3 flex-col">
+              <Link href="/" onClick={() => setIsOpen(!isOpen)}>
+                <NavItem>
+                  {currentPath === "" ? (
+                    <span className="text-secondary">HOME</span>
+                  ) : (
+                    "HOME"
+                  )}
+                </NavItem>
+              </Link>
+              <Link href="/store" onClick={() => setIsOpen(!isOpen)}>
+                <NavItem>
+                  {" "}
+                  {currentPath === "store" ? (
+                    <span className="text-secondary">STORE</span>
+                  ) : (
+                    "STORE"
+                  )}
+                </NavItem>
+              </Link>
               <NavItem>
                 {" "}
-                {currentPath === "store" ? (
-                  <span className="text-secondary">STORE</span>
+                {currentPath === "contact" ? (
+                  <span className="text-secondary">CONTACT</span>
                 ) : (
-                  "STORE"
+                  "CONTACT"
                 )}
               </NavItem>
-            </Link>
-            <NavItem>
-              {" "}
-              {currentPath === "contact" ? (
-                <span className="text-secondary">CONTACT</span>
-              ) : (
-                "CONTACT"
-              )}
-            </NavItem>
-            <Link href="/cart">
-              <NavItem>
-                {" "}
-                {currentPath === "cart" ? (
-                  <span className="text-secondary">CART</span>
-                ) : (
-                  "CART"
-                )}
-              </NavItem>
-            </Link>
-          </ul>
+              <Link href="/cart" onClick={() => setIsOpen(!isOpen)}>
+                <NavItem>
+                  {" "}
+                  {currentPath === "cart" ? (
+                    <span className="text-secondary">CART</span>
+                  ) : (
+                    "CART"
+                  )}
+                </NavItem>
+              </Link>
+            </ul>
+          </Transition>
         </div>
       </div>
     </>
